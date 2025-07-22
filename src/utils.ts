43df466576426170169053970e12476b99715d19
0xtz <0xtz.52@gmail.com>
2025-07-22 22:48:09 +0100

    - vitest for testing - CI script

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+/**
+ * Add two numbers together
+ * @param a - The first number
+ * @param b - The second number
+ * @returns The sum of the two numbers
+ */
+export function add(a: number, b: number) {
+  return a + b;
+}
